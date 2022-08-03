732bae1568fc486a9d768c575e444dfbe0dd9162
Wei Chen <victoriaweichen7@gmail.com>
2022-08-03 10:43:53 -0400

    Create __init__.py

---
+++ EquityLens/__init__.py
@@ -0,0 +1,4 @@
+# This is so that you can import ppack or import average from ppack
+# in stead of from ppack.functions import average
+
+from .functions import custom_formatwarning, color_it, compute_hhi, company_dei_score, sample_dei_score, compute_score, Equity_Lens, top_10, counterfactual_transformation, ttest_var
